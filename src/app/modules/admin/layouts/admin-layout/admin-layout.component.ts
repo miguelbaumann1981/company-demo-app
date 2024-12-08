370b8e41b82cf10588dffae6c364aef3ef00b8bd
MiguelDev <miguelmartinezm@outlook.es>
2024-12-08 17:07:10 +0100

    add subtitle

--- src/app/modules/admin/layouts/admin-layout/admin-layout.component.ts
+++ src/app/modules/admin/layouts/admin-layout/admin-layout.component.ts
@@ -1,19 +1,18 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SideMenuComponent } from "../../../shared/components/side-menu/side-menu.component";
 import { MbdevSideMenuComponent, TitleColor } from 'mbdev-side-menu';
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [RouterOutlet, SideMenuComponent, MbdevSideMenuComponent],
+  imports: [RouterOutlet, MbdevSideMenuComponent],
   templateUrl: './admin-layout.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class AdminLayoutComponent {
 
     isAuthenticated = signal(false);
-    TitleColor = signal(TitleColor.blue)
+    TitleColor = signal(TitleColor.blue);
 
     onLogin() {
       this.isAuthenticated.set(true);
